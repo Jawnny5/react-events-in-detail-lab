4f52e6cf741e0e1bdd3d297d56515f9152a14418
“Jawnny5” <digitalanalogbros@gmail.com>
2021-04-22 18:21:43 -0600

    Done

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default function CoordinatesButton(props) {
+  
+  const handleClick = (event) =>{
+    const Coords = [event.clientX, event.clientY]
+    const onReceiveCoordinates = props.onReceiveCoordinates
+    onReceiveCoordinates(Coords)
+  }
+  return (
+    <div>
+      <button onClick={handleClick}></button>
+    </div>
+  )
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+export default function DelayedButton(props) {
+  
+  
+  const handleClick = (event) => {
+    event.persist()
+    const onDelayedClick = props.onDelayedClick
+   setTimeout(() => {
+     onDelayedClick(event), props.delay
+   })
+  } 
+  return (
+    <div>
+      <button onClick={handleClick}></button>
+    </div>
+  )
+}
